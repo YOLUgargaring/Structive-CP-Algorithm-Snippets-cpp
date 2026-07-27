00d142965ab255a6d56ee63a1e28cd3ac60c074d
unknown <1131471716@qq.com>
2026-07-27 14:02:42 +0800

    质数筛扩展质因数分解功能

--- src/数学/线性质数筛-欧拉筛.cpp
+++ src/数学/线性质数筛-欧拉筛.cpp
@@ -5,14 +5,17 @@
 using namespace std;
 
 //模板原题: 洛谷P3383 Link: https://www.luogu.com.cn/problem/P3383
-struct LinearSieve{//线性质数筛-欧拉筛
+class LinearSieve{//线性质数筛-欧拉筛
+private:
     int n;
-    vector<bool>isprime;
-    vector<int>prime;
+    vector<bool>isprime;//判断是否是质数
+    vector<int>spf;//每个数的最小质因数
+    vector<int>prime;//1-n内的质数
 
-    LinearSieve(int maxn){//建立[1,n]内的质数表
-        n=maxn;
+public:
+    LinearSieve(int maxn):n(maxn){//建立[1,n]内的质数表
         isprime.assign(n+1,true);
+        spf.assign(n+1,0);
         build();
     }
 
@@ -20,12 +23,16 @@ struct LinearSieve{//线性质数筛-欧拉筛
         if(n>=0) isprime[0]=false;
         if(n>=1) isprime[1]=false;
         for(int i=2;i<=n;i++) {
-            if(isprime[i]) prime.push_back(i);
+            if(isprime[i]){
+                spf[i]=i;
+                prime.push_back(i);
+            }
             for(int j=0;j<(int)prime.size();j++) {
                 int p=prime[j];
                 i64 product=(i64)i*p;
                 if(product>n) break;
                 isprime[product]=false;
+                spf[product]=p;
                 if(i%p==0) break;
             }
         }
@@ -39,8 +46,22 @@ struct LinearSieve{//线性质数筛-欧拉筛
         return prime[k-1];
     }
 
-    int queryPrimeCount(){//返回[1,n]内质数的总数
+    int queryPrimeCount(){//查询[1,n]内质数的总数
         return prime.size();
+    }
+
+    int querySPF(int x){//查询x的最小质因数
+        return spf[x];
+    }
+
+    vector<int> queryFac(int x){//质因数分解x并返回x的质因数
+        vector<int>fac;
+        while(x>1){
+            int p=spf[x];
+            fac.push_back(p);
+            while(x%p==0) x/=p;
+        }
+        return fac;
     }
 };
 
