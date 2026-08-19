df395b5097b200edc6239c7640bbdbf1fc60acf6
unknown <1131471716@qq.com>
2026-08-19 15:05:35 +0800

    后缀数组和后缀自动机加上对字符和整数序列的兼容泛型

--- src/字符串/后缀数组SA-朴素排序倍增法.cpp
+++ src/字符串/后缀数组SA-朴素排序倍增法.cpp
@@ -8,21 +8,28 @@ using namespace std;
     模板例题: 此源代码的solve()解决的是洛谷P3809
     1.后缀排序模板: 洛谷P3809 Link: https://www.luogu.com.cn/problem/P3809
     2.求按字典序排序的循环同构: 洛谷P4051 Link: https://www.luogu.com.cn/problem/P4051
-    3.求字符串的本质不同子串个数: SPOJ Link: https://vjudge.net/problem/SPOJ-SUBST1
+    3.求字符串的本质不同子串个数: SPOJ-705 Link: https://vjudge.net/problem/SPOJ-SUBST1
 */
-struct SuffixArray{//后缀数组
+/* - ↓↓↓ - The Suffix Array Template - ↓↓↓ - */
+template<typename T> struct SuffixArray{//后缀数组
     int n;//字符串长度
-    string S;//构建后缀数组的对象字符串S,1-based
+    vector<T>S;//构建后缀数组的对象字符串S,1-based
     vector<int>sa;//后缀数组: sa[i]为将所有后缀排序后第i小的后缀的编号
     vector<int>rank;//后缀排名: rank[i]为第i个后缀的排名
     vector<int>rkcopy;//后缀排序辅助数组
     vector<int>height;//height[i]=LCP(sa[i],sa[i-1]): 第i名后缀与第i-1名后缀的最长公共前缀的长度(height[1]=0)
-    const char apart='#';//不计入后缀数组和后缀排序的分隔符(sa[i]=rank[i]=0不影响字典序和排序)
+    const T apart=0;//不计入后缀数组和后缀排序的分隔符(sa[i]=rank[i]=0不影响字典序和排序)
 
     SuffixArray(){}
 
-    SuffixArray(const string& s){//构建字符串s的后缀数组
-        S=apart+s;
+    static vector<char> stringToVecChar(string s){
+        vector<char>vec(s.begin(),s.end());
+        return vec;
+    }
+
+    SuffixArray(const vector<T>& s){//构建字符串s的后缀数组
+        S.push_back(0);
+        for(auto v:s) S.push_back(v);
         n=s.size();
         sa.assign(n+1,0);
         height.assign(n+1,0);
@@ -35,6 +42,8 @@ struct SuffixArray{//后缀数组
         }
         build();
     }
+    template<typename U=T,typename enable_if<is_same<U,char>::value,int>::type=0>
+    SuffixArray(const string&s):SuffixArray(stringToVecChar(s)){}
 
     void build(){//后缀数组和后缀排序构建
         for(int i=1;i<n;i<<=1){
@@ -63,12 +72,15 @@ struct SuffixArray{//后缀数组
         return res;
     }
 };
+using SAchar=SuffixArray<char>;
+using SAint=SuffixArray<int>;
+/* - ↑↑↑ - The Suffix Array Template - ↑↑↑ - */
 
 /*=============...solving...=============*/
 void solve_by_YOLU(){
     string s;
     cin>>s;
-    SuffixArray SA(s);
+    SAchar SA(s);
     for(int i=1;i<=SA.n;i++) cout<<SA.sa[i]<<" ";
 } 
 /*=============...solving...=============*/
--- src/字符串/后缀自动机SAM.cpp
+++ src/字符串/后缀自动机SAM.cpp
@@ -16,13 +16,15 @@ using namespace std;
     8.查询不包含某个字符的本质不同子串个数: ABC452G Link: https://atcoder.jp/contests/abc452/tasks/abc452_g
     9.求字符串的最小表示(字典序最小的循环同构): 洛谷P1368 Link: https://www.luogu.com.cn/problem/P1368
     10.后缀链接树及其DFS欧拉序运用辅助求解困难区间子串问题: 洛谷P4770 Link: https://www.luogu.com.cn/problem/P4770
-    注: 上述 2,9 两个题目并不是对真实字符串构建SAM,而是对正整数序列构建,需稍微调整节点和方法的参数类型,但算法思想相同
+    注: 本模板通过模板参数T兼容char和整数类型,可分别使用SAMchar和SAMint
 */
-struct SAM{//后缀自动机
+/* - ↓↓↓ - The Suffix Automaton Template - ↓↓↓ - */
+template<typename T> struct SAM{//后缀自动机
+    using query_result=typename conditional<is_same<T,char>::value,string,vector<T>>::type;
     struct node{//状态节点
         int len;//该状态对应的最长字串长度
         int link;//该状态的后缀链接
-        map<char,int>nxt;//转移列表
+        map<T,int>nxt;//转移列表
         vector<int>gcnt;//当前状态里所有子串在每个字符串中的出现次数(gcnt[i]对应第i个字符串),用于字符串拼接的伪广义SAM
     };
     vector<node>state;//状态节点池
@@ -40,27 +42,68 @@ struct SAM{//后缀自动机
     bool ordBuilt;//标记是否已经构建了按len[v]从大大小排序的状态序列
     bool cntBuilt;//标记是否已经完成了查询子串出现次数所需的cnt构建
     bool dpBuilt;//标记是否已经完成了查询字典序第k小子串所需的DP构建
-    bool gcntBuilt;//标记是否已经完成了查询子串在每个字符串的出现次数所需的gcnt构建
+    bool gcntBuilt;//标记是否已经完成了查询子串在每个字符串中的出现次数所需的gcnt构建
     const string KthSubNotfound="No such line.";//找不到第k小子串输出信息,可直接在此处修改
 
     SAM(){}
 
-    SAM(string s){//构造字符串s的SAM
+    static vector<char>stringToVecChar(string s){
+        vector<char>vec(s.begin(),s.end());
+        return vec;
+    }
+
+    static vector<vector<char>>stringToVecChar(const vector<string>&str){
+        vector<vector<char>>vec(str.size());
+        for(int i=0;i<str.size();i++) vec[i]=stringToVecChar(str[i]);
+        return vec;
+    }
+
+    static vector<T>getSeparators(const vector<vector<T>>&str){
+        set<T>used;
+        for(const auto&v:str) for(const auto&c:v) used.insert(c);
+        vector<T>separator;
+        T candidate=numeric_limits<T>::lowest();
+        for(int i=0;i<str.size();i++){
+            while(used.count(candidate)){
+                if(candidate==numeric_limits<T>::max()){
+                    cerr<<"Separator Type Error!"<<endl;
+                    exit(0);
+                }
+                candidate++;
+            }
+            separator.push_back(candidate);
+            used.insert(candidate);
+            if(candidate!=numeric_limits<T>::max()) candidate++;
+        }
+        return separator;
+    }
+
+    static query_result convertResult(const vector<T>&res){
+        if constexpr(is_same<T,char>::value) return string(res.begin(),res.end());
+        else return res;
+    }
+
+    SAM(const vector<T>&s){//构造字符串或整数序列s的SAM
         int n=s.size();
         initEmpty(n,0);
         for(const auto&c:s) extend(c);
     }
+    template<typename U=T,typename enable_if<is_same<U,char>::value,int>::type=0>
+    SAM(const string&s):SAM(stringToVecChar(s)){}
 
-    SAM(vector<string> str){//构造字符串数组str和分隔符拼接的伪广义SAM,str大小不能超过64
+    SAM(const vector<vector<T>>&str){//构造序列数组str和分隔符拼接的伪广义SAM
         int m=str.size();
         int totL=m;
         for(int i=0;i<m;i++) totL+=str[i].size();
+        vector<T>separator=getSeparators(str);
         initEmpty(totL,m);
         for(int i=0;i<m;i++){
             for(const auto&c:str[i]) extend(c,i);
-            extend(char(i+1));
+            extend(separator[i]);
         }
     }
+    template<typename U=T,typename enable_if<is_same<U,char>::value,int>::type=0>
+    SAM(const vector<string>&str):SAM(stringToVecChar(str)){}
 
     void initEmpty(int n,int m){//构造只有初始节点的空SAM,m为构建字符串拼接式伪广义SAM的字符串个数,不构建伪广义SAM可传入0
         maxn=2*n+1;
@@ -78,7 +121,7 @@ struct SAM{//后缀自动机
         dpBuilt=false;
     }
 
-    int extend(char c){//在字符串末尾新增一个字符更新SAM,并返回当前的最后一个状态
+    int extend(T c){//在字符串或整数序列末尾新增一个字符或元素更新SAM,并返回当前的最后一个状态
         int cur=siz++;
         state[cur].len=state[last].len+1;
         int p=last;
@@ -109,14 +152,13 @@ struct SAM{//后缀自动机
         ordReset();
         return cur;
     }
-
-    int extend(char c,int id){//带编号插入字符,用于构建字符串拼接式伪广义SAM
+    int extend(T c,int id){//带编号插入字符或元素,用于构建字符串拼接式伪广义SAM
         int cur=extend(c);
         state[cur].gcnt[id]++;
         return cur;
     }
 
-    //字符串末尾新增字符后状态更新,再次调用buildOrd,buildCnt,buildDP需要重置ord,cnt,dp
+    //字符串或整数序列末尾新增字符或元素后状态更新,再次调用buildOrd,buildCnt,buildDP需要重置ord,cnt,dp
     void ordReset(){
         ordBuilt=false;
         cntBuilt=false;
@@ -195,11 +237,11 @@ struct SAM{//后缀自动机
         dfs(0);
     }
 
-    int querySubNum(){//查询当前本质不同子串的个数
+    int querySubNum(){//查询当前本质不同 子串/连续非空子序列 的个数
         return differentSub;
     }
 
-    int querySub_withoutBan(char ban){//查询当前所有子串中不包含字符ban的本质不同子串个数
+    int querySub_withoutBan(T ban){//查询当前所有子串/连续非空子序列中不包含字符或元素ban的本质不同 子串/连续非空子序列 个数
         buildOrd();
         vector<int>tdp(siz);
         for(int v:ord){
@@ -213,80 +255,97 @@ struct SAM{//后缀自动机
         return tdp[0];
     }
 
-    int queryOCC(string t){//查询模式串t在主串s中的出现次数
+    int queryOCC(const vector<T>&t){//查询模式字符串或整数连续非空序列t在主字符串或整数序列中的出现次数
         int v=0;
-        for(char c:t){
+        for(const auto&c:t){
             if(!state[v].nxt.count(c))
                 return 0;
             v=state[v].nxt[c];
         }
         return cnt[v];
     }
+    template<typename U=T,typename enable_if<is_same<U,char>::value,int>::type=0>
+    int queryOCC(const string&t){//查询模式串t在主串中的出现次数
+        return queryOCC(stringToVecChar(t));
+    }
 
-    //查询字符串的第K小子串: type=0 查询本质不同第K小;type=1 查询位置不同第K小
+    //查询字符串或整数序列的第K小子串:type=0 查询本质不同第K小;type=1 查询位置不同第K小
     //若有多次s的第k小子串查询推荐显式构造 SAM sam(s) 后调用此接口 sam.queryKthSub(k,type)
-    string queryKthSub(int k,int type){
-        string res="";
+    query_result queryKthSub(int k,int type){
+        vector<T>res;
         buildDP();
         if(type==0){
-            if(dp[0]<k) return KthSubNotfound;
+            if(dp[0]<k){
+                if constexpr(is_same<T,char>::value) return KthSubNotfound;
+                else return {};
+            }
             int v=0;
             while(1){
                 int wv=(v==0)?0:1;
-                if(k<=wv) return res;
+                if(k<=wv) return convertResult(res);
                 k-=wv;
                 bool found=false;
                 for(const auto&[c,u]:state[v].nxt){
                     if(k>dp[u]) k-=dp[u];
                     else{
-                        res+=c;
+                        res.push_back(c);
                         v=u;
                         found=true;
                         break;
                     }
                 }
-                if(!found) return KthSubNotfound;
+                if(!found){
+                    if constexpr(is_same<T,char>::value) return KthSubNotfound;
+                    else return {};
+                }
             }
         }
         else if(type==1){
-            if(dpL[0]<k) return KthSubNotfound;
+            if(dpL[0]<k){
+                if constexpr(is_same<T,char>::value) return KthSubNotfound;
+                else return {};
+            }
             int v=0;
             while(1){
                 int wv=(v==0)?0:cnt[v];
-                if(k<=wv) return res;
+                if(k<=wv) return convertResult(res);
                 k-=wv;
                 bool found=false;
                 for(const auto&[c,u]:state[v].nxt){
                     if(k>dpL[u]) k-=dpL[u];
                     else{
-                        res+=c;
+                        res.push_back(c);
                         v=u;
                         found=true;
                         break;
                     }
                 }
-                if(!found) return KthSubNotfound;
+                if(!found){
+                    if constexpr(is_same<T,char>::value) return KthSubNotfound;
+                    else return {};
+                }
             }
         }
         else{
             cerr<<"Query Type Error!"<<endl;
             exit(0);
         }
-        return KthSubNotfound;
+        if constexpr(is_same<T,char>::value) return KthSubNotfound;
+        else return {};
     }
 
-    //对外接口: 查询两个字符串的最长公共子串LCS
-    //该接口属于 SAM 类本身,可直接调用 SAM::queryLCS(s,t) 进行LCS查询
-    static pair<string,int> queryLCS(const string &s,const string &t){
-        SAM sam(s);
+    //对外接口: 查询两个字符串或整数序列的最长公共子串
+    //该接口属于 SAM 类本身,可直接调用 SAMchar::queryLCS(s,t)或SAMint::queryLCS(s,t) 进行LCS查询
+    static pair<vector<T>,int> queryLCS(const vector<T>&s,const vector<T>&t){
+        SAM<T>sam(s);
         int lenLCS=0;
         int endpos=0;
         int v=0,l=0;
         for(int i=0;i<t.size();i++){
-            char c=t[i];
+            const T&c=t[i];
             while(v!=-1&&!sam.state[v].nxt.count(c)){
                 v=sam.state[v].link;
-                l=sam.state[v].len;
+                if(v!=-1) l=sam.state[v].len;
             }
             if(v==-1){
                 v=0;
@@ -300,16 +359,23 @@ struct SAM{//后缀自动机
                 endpos=i;
             }
         }
-        string LCS=t.substr(endpos-lenLCS+1,lenLCS);
+        if(lenLCS==0) return {{},0};
+        vector<T>LCS(t.begin()+endpos-lenLCS+1,t.begin()+endpos+1);
         return {LCS,lenLCS};
     }
+    template<typename U=T,typename enable_if<is_same<U,char>::value,int>::type=0>
+    static pair<string,int> queryLCS(const string&s,const string&t){
+        auto res=queryLCS(stringToVecChar(s),stringToVecChar(t));
+        return {string(res.first.begin(),res.first.end()),res.second};
+    }
 
-    //对外接口: 查询多个字符串的共同最长公共子串LCS
-    //该接口属于 SAM 类本身,可直接调用 SAM::queryMultiLCS(str) 进行多串LCS查询
-    static int queryMultiLCS(const vector<string>&str){
-        string base;
+    //对外接口: 查询多个字符串或整数序列的共同最长公共子串
+    //该接口属于 SAM 类本身,可直接调用 SAMchar::queryMultiLCS(str)或SAMint::queryMultiLCS(str) 进行多串LCS查询
+    static int queryMultiLCS(const vector<vector<T>>&str){
+        if(str.empty()) return 0;
+        vector<T>base;
         int minlen=INT_MAX/2;
-        int idx;
+        int idx=0;
         for(int i=0;i<str.size();i++){
             if(str[i].size()<minlen){
                 base=str[i];
@@ -317,7 +383,7 @@ struct SAM{//后缀自动机
                 idx=i;
             }
         }
-        SAM sam(base);
+        SAM<T>sam(base);
         sam.buildOrd();
         vector<int>best(sam.siz+1);
         vector<int>LCSlen(sam.siz+1);
@@ -325,10 +391,10 @@ struct SAM{//后缀自动机
         for(int i=0;i<str.size();i++){
             if(i==idx) continue;
             fill(best.begin(),best.end(),0);
-            string t=str[i];
+            const vector<T>&t=str[i];
             int v=0,l=0;
             for(int i=0;i<t.size();i++){
-                char c=t[i];
+                const T&c=t[i];
                 while(v!=-1&&!sam.state[v].nxt.count(c)){
                     v=sam.state[v].link;
                     if(v!=-1) l=sam.state[v].len;
@@ -351,51 +417,74 @@ struct SAM{//后缀自动机
         int res=*max_element(LCSlen.begin(),LCSlen.end());
         return res;
     }
+    template<typename U=T,typename enable_if<is_same<U,char>::value,int>::type=0>
+    static int queryMultiLCS(const vector<string>&str){
+        vector<vector<char>>t=stringToVecChar(str);
+        return queryMultiLCS(t);
+    }
 
-    //对外接口: 查询字符串的 本质不同第K小字典序子串(参数type=0) 或 查询位置不同第K小字典序子串(参数type=1)
-    //该接口属于 SAM 类本身,可直接调用 SAM::queryKthSub(s,k,type) 进行s的字典序第k小子串查询
+    //对外接口: 查询字符串或整数序列的 本质不同第K小字典序子串(参数type=0) 或 查询位置不同第K小字典序子串(参数type=1)
+    //该接口属于 SAM 类本身,可直接调用 SAMchar::queryKthSub(s,k,type)或SAMint::queryKthSub(s,k,type) 进行s的字典序第k小子串查询
     //若只有单次s的查询第k小子串推荐调用此接口
-    static string queryKthSub(string s,int k,int type){
+    static query_result queryKthSub(vector<T>s,int k,int type){
         if(type!=0&&type!=1){
             cerr<<"Query Type Error!"<<endl;
             exit(0);
         }
-        SAM sam(s);
+        SAM<T>sam(s);
+        query_result res=sam.queryKthSub(k,type);
+        return res;
+    }
+    template<typename U=T,typename enable_if<is_same<U,char>::value,int>::type=0>
+    static string queryKthSub(string s,int k,int type){
+        SAM<char>sam(s);
         string res=sam.queryKthSub(k,type);
         return res;
     }
 
-    //对外接口: 查询字符串的最小表示,即 字典序最小的循环同构
-    //该接口属于 SAM 类本身,可直接调用 SAM::queryMinRepresent(s) 进行s的最小表示查询
-    static string queryMinRepresent(const string &s){
-        string t=s+s;
-        SAM sam(t);
+    //对外接口: 查询序列的最小表示,即 字典序最小的循环同构
+    //该接口属于 SAM 类本身,可直接调用 SAMchar::queryMinRepresent(s)或SAMint::queryMinRepresent(s) 进行s的最小表示查询
+    static query_result queryMinRepresent(const vector<T>&s){
+        vector<T>t=s;
+        t.insert(t.end(),s.begin(),s.end());
+        SAM<T>sam(t);
         sam.buildOrd();
         vector<int>pdp(sam.siz);
         for(int v:sam.ord){
             for(const auto&[c,u]:sam.state[v].nxt) pdp[v]=max(pdp[v],pdp[u]+1);
         }
-        string res="";
+        vector<T>res;
         int v=0;
         for(int i=0;i<s.size();i++){
             int remain=s.size()-i-1;
             for(const auto&[c,u]:sam.state[v].nxt){
                 if(pdp[u]>=remain){
-                    res+=c;
+                    res.push_back(c);
                     v=u;
                     break;
                 }
             }
         }
-        return res;
+        return convertResult(res);
+    }
+    template<typename U=T,typename enable_if<is_same<U,char>::value,int>::type=0>
+    static string queryMinRepresent(const string&s){
+        return queryMinRepresent(stringToVecChar(s));
     }
 };
+template<typename T>SAM(const vector<T>&)->SAM<T>;
+SAM(const string&)->SAM<char>;
+template<typename T>SAM(const vector<vector<T>>&)->SAM<T>;
+SAM(const vector<string>&)->SAM<char>;
+using SAMchar=SAM<char>;
+using SAMint=SAM<int>;
+/* - ↑↑↑ - The Suffix Automaton Template - ↑↑↑ - */
 
 /*=============...solving...=============*/
 void solve_by_YOLU(){
     string s;
     cin>>s;
-    SAM sam(s);
+    SAMchar sam(s);
     sam.buildCnt();
     int ans=0;
     for(int v=1;v<=sam.siz;v++){
